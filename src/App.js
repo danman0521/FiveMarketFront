37ffc414ca90ebeef4b7f548be2a1acd637cbb99
danman0521 <114002528+danman0521@users.noreply.github.com>
2022-12-19 19:50:58 -0500

    Primer commit
    primer commita FiveMarket front

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Components/Login';
+import CreateAccount from './Components/CreateAccount';
+import Admin from './Components/Admin';
+import Home from './Components/Home';
+import CreateCategory from './Components/CreateCategory';
+import UpdateCategory from './Components/UpdateCategory';
+import HomeProducts from './Components/Products/HomeProducts';
+import CreateProduct from './Components/Products/CreateProduct';
+import UpdateProduct from './Components/Products/UpdateProducts';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/Create-Account" exact element={< CreateAccount />} />
+        <Route path="/Login" exact element={< Login />} />
+        <Route path="/admin" exact element={< Admin />} />
+
+        <Route path="/crear-categorias" exact element={< CreateCategory />} />
+
+        <Route path="/update-category/:idCategoria" exact element={< UpdateCategory />} />
+
+        <Route path="/update-product/:idProduct" exact element={< UpdateProduct/>} />
+
+        <Route path="/home-products/:idCategoria" exact element={< HomeProducts />} />
+        
+        <Route path="/create-product/:idCategoria" exact element={<CreateProduct />} />
+      </Routes>
+
+    </Router>
+
+
   );
 }
-
 export default App;
---
+++ src/Components/Products/ViewProducts.js
@@ -0,0 +1,44 @@
+import React from "react";
+//import UpdateProduct from "./UpdateProducts";
+import { Link } from "react-router-dom";
+export const ViewProducts = ({ product }) => {
+    //console.log("hola producto", product)
+    const { nombre, descripcion, stock, precio, imagen } = product;
+    
+    return (
+        <div
+            className="border-r border-4 border-red-500 rounded-lg  bg-white p-5 flex justify-between  items-center"
+        >
+            <div className=" flex-col items-start">
+                <p className="mb-1 text-xl text-black-50 ">nombre:{nombre}</p>
+                <p className="mb-1 text-xl text-black-50  uppercase ">descripcion:{descripcion}</p>
+                <p className="mb-1 text-xl text-black-50  ">stock:{stock}</p>
+                <p className="mb-1 text-xl text-black-50  ">precio:{precio}</p>
+                <img 
+                className="border-4 border-red-500 rounded-lg"
+                
+                src={imagen} width="150" height="150"></img>
+            </div>
+            <div className=" = flex flex-col lg:flex-row gap-2">
+                <Link
+
+            className="bg-indigo-600 px-4 py-3 text-white uppercase font-bold text-sm rounded-lg"
+               to={`/update-product/${product._id}`}
+                //onClick={UpdateProduct(product._id)}
+                
+                >Editar
+
+                </Link>
+                
+                <button
+
+                className="bg-red-600 px-4 py-3 text-white uppercase font-bold text-sm rounded-lg "
+                
+                >Eliminar
+                </button>
+
+            </div>
+        </div>
+    )
+}
+export default ViewProducts;
